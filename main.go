29cf15c990687407d966fc01f8ca6be1bfc4cf7d
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-09-16 15:04:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"net/http"
 	"os"
-  "proxer/client"
+	"proxer/client"
 	"github.com/gorilla/handlers"
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("- Starting PROXER -")
+	fmt.Println("- Starting PROXER -")
 	http.Handle("/", handlers.CombinedLoggingHandler(os.Stdout, client.New()))
 	http.ListenAndServe(":8080", nil)
-  fmt.Println("- Proxer STARTED on port 8080 -")
 }
